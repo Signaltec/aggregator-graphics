ace97b9f48b13e582e10827196400079cc25196a
ildar galiev <ildargaliev@rfc1918.private.ip.localhost>
2015-12-04 12:46:11 +0300

    slider + port micro chart together

--- app.js
+++ app.js
@@ -180,14 +180,13 @@ function PortsCharts(container, color) {
 
 /* ----------------------------------------------------- */
 
-var app = angular.module("app",['ngAnimate','ngRoute']);
+var app = angular.module("app",['ngAnimate','ngRoute','ngSanitize','uiSlider']);
 
 app.controller("AppCtrl", function($rootScope, $window, $location, $filter, $http, $timeout) {
 
 });
 
-app.controller("aggregatorGraphicsCtrl", function($scope, $location, $route, $window) {
-  
+app.controller("aggregatorGraphicsCtrl", function($scope, $window, $timeout) {
   
   function getPorts() {
     var count = 10;
@@ -202,6 +201,20 @@ app.controller("aggregatorGraphicsCtrl", function($scope, $location, $route, $wi
     }
     return result;
   }
+  
+  /* TODO: make directive if possible */
+  function drawSelection() {
+    var mc = document.querySelector('.ports-microcharts');
+    var selection = document.querySelector('.ports-microcharts .selection');
+    
+    var pointers = document.querySelectorAll('slider .pointer');
+    if (pointers.length > 1) {
+      selection.style.top = 0;
+      selection.style.height = mc.offsetHeight + 'px';
+      selection.style.left = (pointers[0].offsetLeft + 80) + 'px';
+      selection.style.width = (pointers[1].offsetLeft - pointers[0].offsetLeft) + 'px';
+    }
+  }  
   
   $scope.toggleEmpty = function() {
     $scope.ports.map(function(i) {
@@ -219,11 +232,35 @@ app.controller("aggregatorGraphicsCtrl", function($scope, $location, $route, $wi
   };
   
   // Init
+  $scope.timeNav = {
+    start: -40,
+    end: 0,
+    floor: -366,
+    ceiling: 0,
+    /* My own properties */
+    now: new Date(),
+    hour: 60 * 60 * 1000,
+    format: "D MMM",
+    formatDate: function(delta) {
+      var res = new Date(this.now - (-delta * this.hour));
+      res = moment(res).format(this.format); 
+      return res;
+    }
+  };  
+
+  $scope.timeNavChange = function() {
+    console.log('changed');
+    drawSelection();
+  };
   
   $scope.ports = getPorts();
   $scope.portColors = d3.scale.category20();
   $scope.portColors.domain( $scope.ports.map(function(i) {return i.name;}) );
   
+  // Wait 500 ms before show selection  
+  $timeout(function() {
+    drawSelection();
+  }, 500);  
 });
 
 // microPortChart
@@ -255,8 +292,36 @@ app.directive("microPortChart", function() {
   }
 });
 
+app.directive("timeAxis", function() {
+  var hour = 60 * 60 * 1000;
 
+  return {
+    restrict: "E",
+    scope: {
+      floor: "=",
+      now: "="
+    },
+    link: function(scope, element, attrs) {
+      
+      var width = element[0].offsetWidth;
+      var height = element[0].offsetHeight;
+      var lastDate = new Date(scope.now - (-scope.floor * hour));
+      
+      var x = d3.time.scale().range([0, width]);
+      x.domain([lastDate, scope.now]);
 
+      var xAxis = d3.svg.axis().scale(x).orient("bottom")
+        .tickFormat(d3.time.format("%e %b"));
+      
+      var svg = d3.select(element[0])
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height);
+      
+      svg.append("g").attr("class", "axis").call(xAxis);      
+    }
+  }
+});
 
 
 
