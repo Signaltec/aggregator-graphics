04225c950f42ec6d2896fe1de601d33818850d58
ildar galiev <ildargaliev@rfc1918.private.ip.localhost>
2016-08-17 16:37:52 +0300

    remove _trash influx move outside and rename

--- influx/fakes/fakes.sh
+++
@@ -1,8 +0,0 @@
-# generate
-node fakes.js
-
-# drop
-curl -i -XGET 'http://localhost:8086/query?db=monitoring&q=drop%20measurement%20traffic'
-
-# load data
-curl -i -XPOST 'http://localhost:8086/write?db=monitoring' --data-binary @fakes.txt
